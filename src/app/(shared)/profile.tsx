7f12e06b8104d8a61ac370839ed9eca8904bac9e
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-04-26 21:43:48 +0300

    del comments

--- src/app/(shared)/profile.tsx
+++ src/app/(shared)/profile.tsx
@@ -2,8 +2,9 @@ import { View } from "react-native";
 import { Profile } from "../../modules/users/screens/profile";
 
 export default function ProfileRoute() {
-    // <Text>Profile</Text>
-  return <View>
-    <Profile/>
-  </View>
+    return (
+        <View>
+            <Profile />
+        </View>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
     presets: [require("nativewind/preset")],
     theme: {
         extend: {
-            // example
-            // gradientColors:{
-            //     top: "#77B5BF",
-            //     bottom: "#7D88AA"
-            // }
             colors: {
                 border: "#77B5BF",
             },
