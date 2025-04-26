34a4d2e1b395071dd781d0bbc3749edebda6b9e7
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-04-26 21:42:03 +0300

    finish styles on profile screen

--- src/modules/users/screens/profile.tsx
+++ src/modules/users/screens/profile.tsx
@@ -1,11 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { COLOR_PALETTE } from "../../../shared/theme/colors";
 import { useColorScheme } from "nativewind";
-import MaskedView from "@react-native-masked-view/masked-view";
 
 export function Profile() {
-    const {colorScheme} = useColorScheme();
+    const { colorScheme } = useColorScheme();
     const colors =
         colorScheme === "light"
             ? ([
@@ -20,41 +19,40 @@ export function Profile() {
         <LinearGradient
             colors={colors}
             start={{ x: 0, y: 0 }}
-            // end={{ x: 1, y: 1 }}
-            // className="flex-1 h-full"
+            className="h-full"
         >
-            <View>
-                <Text className="text-white text-2xl font-semibold">Name</Text>
-                <Text className="text-white text-2xl font-semibold">
-                    У мережі
-                </Text>
+            <View className="flex-row gap-10 py-6 px-2">
+                <View className=" border-white border dark:border-border rounded-full p-2 ">
+                    <Image
+                        className="rounded-full h-[88] w-[88]"
+                        source={require("../assets/avatar.jpg")}
+                    ></Image>
+                </View>
+
+                <View className="justify-center gap-4 ">
+                    <Text className="text-white text-4xl font-semibold ">
+                        Name
+                    </Text>
+                    <Text className="text-white text-2xl font-light">
+                        У мережі
+                    </Text>
+                </View>
             </View>
 
-            <View className="mt-4">
-                {/* <MaskedView
-                    maskElement={
-                        <Text className="font-bold">Градиентный Текст</Text>
-                    }
-                >
-                    <LinearGradient
-                        colors={[COLOR_PALETTE.lightTheme.gradientColors.top, COLOR_PALETTE.lightTheme.gradientColors.bottom]}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 0 }}
-                        // style={{ flex: 1 }}
-                    />
-                    <Text className="font-[10000]">fgdsacdvfb ghn</Text>
-                </MaskedView> */}
+            <View className="mt-4 border m-2 rounded-xl p-2 dark:border-border border-white">
                 <View>
-                    <Text className="text-white">Номер телефону</Text>
-                    <Text className="text-white">+380-99-639-**-**</Text>
+                    <Text className="text-white dark:text-border font-extralight text-2xl">Номер телефону</Text>
+                    <Text className="text-white dark:text-border font-normal text-2xl">+380-99-639-**-**</Text>
                 </View>
+                <View className="h-px w-full bg-white dark:bg-border my-4" />
                 <View>
-                    <Text className="text-white">Про себе</Text>
-                    <Text className="text-white">Хз</Text>
+                    <Text className="text-white dark:text-border font-extralight text-2xl">Про себе</Text>
+                    <Text className="text-white dark:text-border font-normal text-2xl">Хз</Text>
                 </View>
+                <View className="h-px w-full bg-white dark:bg-border my-4" />
                 <View>
-                    <Text className="text-white">Ім'я користувача</Text>
-                    <Text className="text-white">@mila_krutaja</Text>
+                    <Text className="text-white dark:text-border font-extralight text-2xl">Ім'я користувача</Text>
+                    <Text className="text-white dark:text-border font-normal text-2xl">@mila_krutaja</Text>
                 </View>
             </View>
         </LinearGradient>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {
-        // example
-        // gradientColors:{
-        //     top: "#77B5BF",
-        //     bottom: "#7D88AA"
-        // }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            // example
+            // gradientColors:{
+            //     top: "#77B5BF",
+            //     bottom: "#7D88AA"
+            // }
+            colors: {
+                border: "#77B5BF",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
