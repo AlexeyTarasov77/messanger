081f60595feae616dc6a555604e5a9b6029ec324
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-05-02 20:53:22 +0200

    Extended IUser with new fields

--- src/modules/users/types.ts
+++ src/modules/users/types.ts
@@ -9,8 +9,17 @@ export interface IRegisterForm extends ILoginForm {
 }
 
 export interface IUser {
+    id: string;
+    firstName?: string;
+    lastName?: string;
     username: string;
     email: string
+    phoneNumber: string;
+    isOnline: boolean;
+    aboutMe?: string;
+    avatarUrl?: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 export interface IRegisterResponse {
