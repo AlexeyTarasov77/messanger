58c367fb3c09eafc9518342eabcd533765b3695d
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-05-12 19:40:28 +0300

    fix register and merge

--- src/modules/users/screens/register/step-one.tsx
+++ src/modules/users/screens/register/step-one.tsx
@@ -1,7 +1,6 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text } from "react-native";
 import { Button } from "../../../../shared/ui/button";
 import { Input } from "../../../../shared/ui/input";
-import { ICONS } from "../../../../shared/ui/icons";
 import { Link } from "expo-router";
 import { Controller, useForm } from "react-hook-form";
 import { IRegisterStepOne } from "../../types";
--- src/modules/users/screens/register/step-two.tsx
+++ src/modules/users/screens/register/step-two.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text } from "react-native";
 import { Button } from "../../../../shared/ui/button";
 import { Input } from "../../../../shared/ui/input";
 import { Link, useRouter } from "expo-router";
@@ -11,185 +11,97 @@ export function RegisterStepTwo() {
     const router = useRouter();
     const prevData = useLocalSearchParams() as unknown as IRegisterStepOne;
 
-    const { handleSubmit, control, formState } = useForm<IRegisterStepTwo>({
-        defaultValues: { otp: "" },
-    });
+    const { handleSubmit, control, formState } =
+        useForm<IRegisterStepTwo>({
+            defaultValues: {
+                otp1: "",
+                otp2: "",
+                otp3: "",
+                otp4: "",
+                otp5: "",
+                otp6: "",
+            },
+        });
+
+    const otpFieldNames = [
+        "otp1",
+        "otp2",
+        "otp3",
+        "otp4",
+        "otp5",
+        "otp6",
+    ] as const;
+
+    const otpInputs = otpFieldNames.map((fieldName, index) => (
+        <View className="flex-row" key={index}>
+            <Controller
+                control={control}
+                name={fieldName}
+                rules={{
+                    required: {
+                        value: true,
+                        message: "Confirmation otp is required",
+                    },
+                    maxLength: 1,
+                }}
+                render={({ field }) => (
+                    <Input
+                        placeholder="___"
+                        onChange={field.onChange}
+                        onChangeText={field.onChange}
+                        value={field.value}
+                        autoCorrect={false}
+                        className="h-[40] w-8 text-center border-grey rounded-2xl"
+                    />
+                )}
+            />
+            {/* <View>
+                <Text>{formState.errors[fieldName]?.message}</Text>
+            </View> */}
+        </View>
+    ));
+
+    const groupedOtp = [];
+    for (let i = 0; i < otpInputs.length; i += 2) {
+        groupedOtp.push(
+            <View className="flex-row gap-2" key={i}>
+                {otpInputs.slice(i, i + 2)}
+            </View>
+        );
+    }
 
     async function onSubmit(data: IRegisterStepTwo) {
-        await authService.register({ ...data, ...prevData });
+        const otp = otpFieldNames.map((name) => data[name]).join("");
+        console.log("OTP:", otp);
+        await authService.register({  ...prevData, otp });
         router.replace("/");
     }
+
     return (
         <View className="h-full pt-10 bg-mainBg">
-            <View className="self-center items-center rounded-2xl bg-white px-4 py-12">
+            <View className="self-center items-center rounded-2xl bg-white px-4 py-12 w-[90%]">
                 <View>
                     <Text className="text-darkBlue font-medium text-2xl self-center pt-8 pb-4">
                         Підтвердження пошти
                     </Text>
                 </View>
-                <View className="">
+                <View>
                     <Text className="text-darkBlue font-medium text-sm self-center pt-8 pb-4 px-4 text-center">
                         Ми надіслали 6-значний код на вашу пошту
                         (you@example.com). Введіть його нижче, щоб підтвердити
                         акаунт
                     </Text>
                 </View>
-                <View className="self-center w-full ">
-                    <View className="gap-2">
+                <View className="self-center ">
+                    <View className="gap-2 pb-3 ">
                         <View>
                             <Text className="color-darkBlue text-base font-normal">
                                 Код підтвердження
                             </Text>
                         </View>
-                        <View className="flex-row gap-6">
-                            <View className="flex-row gap-2">
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                    
-                                </View>
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                </View>
-                            </View>
-                            <View className="flex-row gap-2">
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                </View>
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                </View>
-                            </View>
-                            <View className="flex-row gap-2">
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                </View>
-                                <View className="flex-row">
-                                    <Controller
-                                        control={control}
-                                        name="otp"
-                                        rules={{
-                                            required: {
-                                                value: true,
-                                                message:
-                                                    "Confirmation otp is required",
-                                            },
-                                        }}
-                                        render={({ field }) => (
-                                            <Input
-                                                placeholder="___"
-                                                onChange={field.onChange}
-                                                onChangeText={field.onChange}
-                                                value={field.value}
-                                                autoCorrect={false}
-                                                className="h-[40]"
-                                            />
-                                        )}
-                                    />
-                                </View>
-                            </View>
+                        <View className="flex-row gap-6 pb-8 ">
+                            {groupedOtp}
                         </View>
-                        <View><Text>{formState.errors.otp?.message}</Text></View>
                     </View>
                     <View>
                         <View>
