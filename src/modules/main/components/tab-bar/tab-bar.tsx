cc82794c7213daabfc7d7fb9af7b25c1fefb3805
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-05-12 17:49:57 +0200

    feat: Improve and refactor tab-bar component
    New TabBar component includes separate array of routes to flexibly
    render them using map saving same style for all routes.

---
+++ src/modules/main/components/tab-bar/tab-bar.tsx
@@ -0,0 +1,26 @@
+import { Tabs } from "expo-router";
+import { tabs } from "./tabs";
+import { StyleProp, TextStyle } from "react-native";
+
+export function TabBar() {
+  const tabBarLabelStyle: StyleProp<TextStyle> = {
+    color: "black",
+    fontWeight: "bold",
+  }
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      {tabs.map(({ name, title, icon: Icon }) => (
+        <Tabs.Screen
+          key={name}
+          name={name}
+          options={{
+            title,
+            tabBarLabelStyle,
+            tabBarIcon: ({ size }) => <Icon width={size} height={size} />,
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+
+}
