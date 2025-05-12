cb0eaad6a0e8ed68ff1dc46819b7dc81302eedaf
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-05-12 21:30:28 +0200

    feat,fix: Returning loader in RedirectUnauthenticated which caused errors before. Other small improvements and fixes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,16 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useColorScheme } from "nativewind";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import React, { useEffect } from "react";
-import { THEME_STORAGE_KEY } from "../shared/constants";
 import "../styles/global.css";
-import { Slot } from "expo-router";
-import { UsersProvider } from "../modules/users/components/users-ctx/context";
 import { RootLayout } from "../modules/main/screens/layouts/root-layout";
+import { UsersProvider } from "../modules/users/components/users-ctx/context";
 
-export default RootLayout
+export default function Layout() {
+  return (
+    <SafeAreaProvider>
+      <UsersProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <RootLayout />
+        </SafeAreaView>
+      </UsersProvider>
+    </SafeAreaProvider>
+  )
+}
--- src/modules/main/components/header/actions.tsx
+++ src/modules/main/components/header/actions.tsx
@@ -1,8 +1,9 @@
 import { TouchableOpacity } from "react-native";
 import { LogOutIcon, PlusIcon, SettingsIcon } from "../../../../shared/ui/icons/headerIcons";
 import { authService } from "../../../users/services";
+import { Router } from "expo-router";
 
-export const headersBaseActions = [
+export const getBaseActions = () => [
   <TouchableOpacity className=" border-text border rounded-full p-2 ">
     <PlusIcon width={20} height={20} />
   </TouchableOpacity>,
@@ -10,9 +11,9 @@ export const headersBaseActions = [
     <SettingsIcon width={20} height={20} />
   </TouchableOpacity>,
 ]
-export const authenticatedOnlyActions = [
+export const getAuthenticatedOnlyActions = (onLogout: () => void) => [
   <TouchableOpacity
-    onPress={() => { authService.logOut(); }}
+    onPress={onLogout}
     className="border-text border rounded-full p-2 "
   >
     <LogOutIcon width={20} height={20} />
