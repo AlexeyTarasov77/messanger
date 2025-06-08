2a63851ba047d6ece32c92aad1d3ed9f4c04deb9
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-06-08 20:11:02 +0200

    Create UI of albums section

---
+++ src/modules/users/components/album.tsx
@@ -0,0 +1,41 @@
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import { Heading } from "../../../shared/ui/heading/heading";
+import { IAlbum } from "../types";
+import { RoundedButton } from "../../../shared/ui/button/button";
+import { ICONS } from "../../../shared/ui/icons";
+
+export function Album({ albumData }: { albumData: IAlbum }) {
+  return (
+    <View className="gap-4">
+      <Heading label={albumData.name} action={
+        <View className="gap-6 flex-row items-center">
+          <RoundedButton icon={<ICONS.EyeIcon width={20} height={20} />} />
+          <TouchableOpacity>
+            <ICONS.PostSettingsIcon width={20} height={20} />
+          </TouchableOpacity>
+        </View>
+      } />
+      <View className="gap-4 flex-row">
+        <Text className="text-darkBlue">{albumData.subject}</Text>
+        <Text className="text-grey">{albumData.year} рiк</Text>
+      </View>
+      <View className="border border-grey" />
+      <Text className="text-darkBlue">Фотографіїї</Text>
+      <View className="flex-row justify-between items-center flex-wrap">
+        {albumData.photos.map(photo => (
+          <View className="relative mt-3" key={photo.id} style={{ flex: 1 }}>
+            <Image source={{ uri: photo.url }} className="rounded-xl" width={160} height={160} />
+            <View className="absolute top-28 left-16 gap-2 flex-row">
+              <RoundedButton filled icon={<ICONS.EyeIcon width={20} height={20} />} />
+              <RoundedButton filled icon={<ICONS.BinIcon width={20} height={20} />} />
+            </View>
+          </View>
+        ))}
+        <View className="mt-3 w-40 h-40 border-dashed border border-grey justify-center items-center">
+          <RoundedButton icon={<ICONS.PlusIcon width={20} height={20} />} />
+        </View>
+
+      </View>
+    </View>
+  )
+}
--- src/modules/users/screens/settings/albums/albums-list.tsx
+++ src/modules/users/screens/settings/albums/albums-list.tsx
@@ -4,23 +4,26 @@ import { Heading } from "../../../../../shared/ui/heading/heading";
 import { ICONS } from "../../../../../shared/ui/icons";
 import { useUserCtx } from "../../../components/users-ctx/context";
 import { Block } from "../block";
+import { Album } from "../../../components/album";
 
 export function AlbumsList() {
   const { user } = useUserCtx()
   if (!user) return
   return (
-    <Block>
+    <View className="gap-3">
       {
         user.albums ?
           user.albums.map(album => (
-            <View key={album.id}></View>
+            <Block key={album.id}><Album albumData={album} /></Block>
           ))
           :
-          <Heading label="Немає ще жодного альбому" action={
-            <RoundedButton icon={<ICONS.PlusIcon width={20} height={20} />} />
-          }
-          />
+          <Block>
+            <Heading label="Немає ще жодного альбому" action={
+              <RoundedButton icon={<ICONS.PlusIcon width={20} height={20} />} />
+            }
+            />
+          </Block>
       }
-    </Block>
+    </View>
   )
 }
