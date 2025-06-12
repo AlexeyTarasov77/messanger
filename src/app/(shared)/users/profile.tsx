3365598ff1151b0cb69ad385bd9852da4549d011
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-13 00:46:34 +0300

    mini fix

---
+++ src/app/(shared)/users/profile.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Profile } from "../../../modules/users/screens";
+
+export default function ProfileRoute() {
+  return (
+    <View>
+      <Profile />
+    </View>
+  );
+}
