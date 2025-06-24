f7277dbe97e9df1d929259f0cfc5685242a45690
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-24 16:03:29 +0300

    add function open album modal in album list

--- src/modules/users/screens/settings/albums/albums-list.tsx
+++ src/modules/users/screens/settings/albums/albums-list.tsx
@@ -5,9 +5,11 @@ import { ICONS } from "../../../../../shared/ui/icons";
 import { useUserCtx } from "../../../components/users-ctx";
 import { Block } from "../block";
 import { Album } from "../../../components/album";
+import { ModalName, useModal } from "../../../../../shared/context/modal";
 
 export function AlbumsList() {
     const { user } = useUserCtx();
+    const { open: openModal } = useModal();
     if (!user) return;
     return (
         <View className="gap-3">
@@ -27,7 +29,7 @@ export function AlbumsList() {
                     label="Немає ще жодного альбому"
                     action={
                         <RoundedButton
-                            // onPress={}
+                            onPress={() => {openModal({ name: ModalName.CREATE_ALBUM })}}
                             icon={<ICONS.PlusIcon width={20} height={20} />}
                         />
                     }
