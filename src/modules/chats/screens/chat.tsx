c7f5ea17648337e04df771c6f0850e14e872c0c8
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-27 16:00:49 +0300

    fix, keyboard not finished

--- src/modules/chats/screens/chat.tsx
+++ src/modules/chats/screens/chat.tsx
@@ -17,6 +17,7 @@ import { chatsService } from "../services/chats";
 import { UserAvatar } from "../../users/components/avatar";
 import { Loader } from "../../../shared/ui/loader/loader";
 import { MessageCard } from "../components";
+import { DEFAULT_AVATAR_URL } from "../../../shared/constants";
 
 export function ChatScreen() {
     const router = useRouter();
@@ -59,13 +60,17 @@ export function ChatScreen() {
     }, [user]);
     if (!chat || !user) return <Loader />;
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? 160 : 20}
-        >
-            <View className="bg-mainBg h-full">
-                <View className="bg-white rounded-xl border-border border mb-6 pb-2 mt-3 flex-1 px-2">
+        <View className="bg-mainBg flex-1">
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                keyboardVerticalOffset={Platform.OS === "ios" ? 160 : 20}
+            >
+            {/* <KeyboardAwareScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                keyboardShouldPersistTaps="handled"
+            > */}
+                <View className="bg-white rounded-xl border-border border mb-6 pb-2 mt-3 px-2 flex-1 overflow-hidden">
                     <View className="border-b border-border pb-2 p-4 pt-3 flex-row justify-between">
                         <View className="flex-row items-center gap-3">
                             <TouchableOpacity
@@ -84,68 +89,85 @@ export function ChatScreen() {
                                     fill="#81818D"
                                 />
                             </TouchableOpacity>
-                            <UserAvatar user={chat!.partner} />
-                            <View>
-                                <Text className="font-medium text-2xl">
-                                    {chat?.partner.first_name}{" "}
-                                    {chat?.partner.last_name}
-                                </Text>
-                            </View>
+
+                            {chat.partner ? (
+                                <UserAvatar
+                                    className="w-12 h-12"
+                                    user={chat.partner}
+                                />
+                            ) : (
+                                <View>
+                                    <Image
+                                        source={{
+                                            uri:
+                                                chat.avatar ||
+                                                DEFAULT_AVATAR_URL,
+                                        }}
+                                        className="rounded-full w-12 h-12"
+                                    />
+                                </View>
+                            )}
+                            <Text className="font-medium text-2xl">
+                                {chat?.partner.first_name}{" "}
+                                {chat?.partner.last_name}
+                            </Text>
                         </View>
-                        <View>
+                        <View className="items-center self-center">
                             <ICONS.PostSettingsIcon width={20} height={20} />
                         </View>
                     </View>
-                    <ScrollView className="pb-8 pt-2 ">
-                        <View className="flex-1">
-                            {Object.entries(
-                                chat.messages
-                                    .slice()
-                                    .reduce((acc, msg) => {
-                                        const date = new Date(
-                                            msg.sent_at
-                                        ).toDateString();
-                                        if (!acc[date]) acc[date] = [];
-                                        acc[date].push(msg);
-                                        return acc;
-                                    }, {} as Record<string, typeof chat.messages>)
-                            ).map(([date, messages]) => (
-                                <View key={date}>
-                                    <View className="items-center my-2 bg-border w-[40%] justify-center self-center rounded-lg px py-2">
-                                        <Text className="text-base text-darkBlue">
-                                            {new Date(date).toLocaleDateString(
-                                                "uk-UA",
-                                                {
-                                                    day: "numeric",
-                                                    month: "long",
-                                                    year: "numeric",
-                                                }
-                                            )}
-                                        </Text>
-                                    </View>
-                                    {messages.map((msg, index) => (
-                                        <MessageCard
-                                            key={`${msg.id}-${index}`}
-                                            msg={{
-                                                ...msg,
-                                                author:
-                                                    msg.author_id === user.id
-                                                        ? user
-                                                        : chat.partner,
-                                            }}
-                                            isOwnMsg={msg.author_id === user.id}
-                                        />
-                                    ))}
+                    <ScrollView
+                        // className="pb-8 pt-2 flex-1"
+                        // keyboardShouldPersistTaps="handled"
+                        className="pt-2 flex-1"
+                        keyboardShouldPersistTaps="handled"
+                        contentContainerStyle={{ paddingBottom: 20 }}
+                    >
+                        {Object.entries(
+                            chat.messages.slice().reduce((acc, msg) => {
+                                const date = new Date(
+                                    msg.sent_at
+                                ).toDateString();
+                                if (!acc[date]) acc[date] = [];
+                                acc[date].push(msg);
+                                return acc;
+                            }, {} as Record<string, typeof chat.messages>)
+                        ).map(([date, messages]) => (
+                            <View key={date}>
+                                <View className="items-center my-2 bg-border w-[40%] justify-center self-center rounded-lg px py-2">
+                                    <Text className="text-base text-darkBlue">
+                                        {new Date(date).toLocaleDateString(
+                                            "uk-UA",
+                                            {
+                                                day: "numeric",
+                                                month: "long",
+                                                year: "numeric",
+                                            }
+                                        )}
+                                    </Text>
                                 </View>
-                            ))}
-                        </View>
+                                {messages.map((msg, index) => (
+                                    <MessageCard
+                                        key={`${msg.id}-${index}`}
+                                        msg={{
+                                            ...msg,
+                                            author:
+                                                msg.author_id === user.id
+                                                    ? user
+                                                    : chat.partner,
+                                        }}
+                                        isOwnMsg={msg.author_id === user.id}
+                                    />
+                                ))}
+                            </View>
+                        ))}
                     </ScrollView>
-                    <View className="flex-row p-6 bottom-0 ">
+                    <View className="flex-row p-4 bottom-0 pr-28 gap-2 items-center">
                         <Input
                             placeholder="Повідомлення"
                             onChangeText={(val) => setMessage(val)}
                             value={message}
-                            className="w-5/6 h-12"
+                            className="w-full h-12"
                         />
                         <View className="flex-row gap-2">
                             <TouchableOpacity className="flex-row items-center justify-center  border-slive border  rounded-full w-10 h-10">
@@ -168,7 +190,8 @@ export function ChatScreen() {
                         </View>
                     </View>
                 </View>
-            </View>
-        </KeyboardAvoidingView>
+                </KeyboardAvoidingView>
+            {/* </KeyboardAwareScrollView> */}
+        </View>
     );
 }
