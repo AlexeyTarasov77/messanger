ea7e1c3706a5c91f65a70fa27ecf998ba10d901a
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-29 15:09:41 +0300

    deploy

--- src/shared/constants/base.ts
+++ src/shared/constants/base.ts
@@ -2,8 +2,8 @@ import { Platform } from "react-native";
 
 // export const SERVER_URL = `http://${Platform.OS == "ios" ? "localhost" : "10.0.2.2"}:8000/api/v1`;
 // export const WS_SERVER_URL = `ws://${Platform.OS == "ios" ? "localhost" : "10.0.2.2"}:8000`
-export const SERVER_URL = "http://192.168.0.115:8000/api/v1"
-export const WS_SERVER_URL = "ws://192.168.0.115:8000"
-export const THEME_STORAGE_KEY = "theme";
-export const AUTH_TOKEN_KEY = "vibenet__authToken";
-export const EXCLUDED_RECOMMENDED_USERS_KEY = "exclude_recommended_users"
+export const SERVER_URL = process.env.SERVER_URL!
+export const WS_SERVER_URL = process.env.WS_SERVER_URL!
+export const THEME_STORAGE_KEY = process.env.THEME_STORAGE_KEY!
+export const AUTH_TOKEN_KEY = process.env.AUTH_TOKEN_KEY!
+export const EXCLUDED_RECOMMENDED_USERS_KEY = process.env.EXCLUDED_RECOMMENDED_USERS_KEY!
